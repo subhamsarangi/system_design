558e06760255f3429fc00eeca549c23d14d9e6af
subhamsarangi <subhamsarangi2016@gmail.com>
2023-06-02 00:08:50 +0530

    simple server

---
+++ python/src/auth/server.py
@@ -0,0 +1,55 @@
+import jwt, datetime, os
+from flask import Flask, request
+from flask_mysqldb import MySQL
+
+server = Flask(__name__)
+mysql = MySQL(server)
+
+server.config["MYSQL_HOST"] = os.environ.get("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.environ.get("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.environ.get("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.environ.get("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.environ.get("MYSQL_PORT")
+
+print(server.config)
+
+@server.route("/login", methods=["POST"])
+def login():
+    auth = request.authorization
+    if not auth:
+        return "missing creds", 401
+    
+    # check db for username and password
+    cur = mysql.connection().cursor()
+    res = cur.execute(
+        "SELECT email, password FROM, user WHERE email =%s", (auth.username,)
+    )
+    if res>0:
+        user_row = cur.fetchone()
+        email = user_row[0]
+        password = user_row[1]
+
+        if auth.username != email or auth.password != password:
+            return "invalid creds", 401
+        else:
+            TOKEN = createJWT(auth.username, os.environ.get("JWT_SECRET"), True)
+            print(TOKEN)
+            return TOKEN 
+    else:
+        return "user not found", 401
+    
+def createJWT(username, secret, authz):
+    """authz True is admin"""
+    return jwt.encode(
+        {
+            "username": username,
+            "exp": datetime.datetime.now(tz=datetime.timezone.utc()+ datetime.timedelta(days=1)),
+            "iat": datetime.datetime.utcnow(),
+            "admin": authz
+        },
+        secret,
+        algorithm="HS256"
+    )
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=5000)
